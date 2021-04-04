f2f9c22878c2ee505c5154c5ff8d1f2746ec5790
Alexis <moulin.alexis@gmail.com>
2021-04-04 14:00:08 -0400

    model tests added

--- ForecastsTests/YahooFinanceTests.swift
+++ ForecastsTests/YahooFinanceTests.swift
@@ -1,17 +1,16 @@
 import XCTest
 @testable import AI_Stock_Forecasts
 
-// Async code testing
-/*
 class YahooFinanceTests: XCTestCase {
 
-    func testDummyAPI() {
-        var stockValues: [Double] = []
-        getHistoricalData(stockSymbol: "AAPL") { results in
-            for result in results {
-                stockValues.append(result.value)
-            }
-        }
+    func testDummyAPI() throws {
+        let bundle: Bundle = Bundle(for: YahooFinanceTests.self)
+        let data: Chart = bundle.decode(Chart.self, from: "YahooFinanceResultsMockup.json")
+        let timestamps = data.chart.result[0].timestamp
+        XCTAssertEqual(timestamps.count, 23)
+        XCTAssertEqual(timestamps[15] - timestamps[14], 86400)
+        let stockValues = data.chart.result[0].indicators.quote[0].close
+        XCTAssertEqual(stockValues.count, 23)
+        XCTAssertGreaterThan(stockValues[14], 123)
     }
 }
-*/

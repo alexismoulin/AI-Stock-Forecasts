4bd1bbeac2402bdf131e38adc2d9b0544622576a
Alexis <moulin.alexis@gmail.com>
2021-04-04 14:56:24 -0400

    added haptics

--- AI Stock Forecasts/Views/ResultScreen/ResultView.swift
+++ AI Stock Forecasts/Views/ResultScreen/ResultView.swift
@@ -1,8 +1,10 @@
 import SwiftUI
+import CoreHaptics
 
 struct ResultView: View {
 
     @State var selectedSegment: Segment?
+    @State private var engine = try? CHHapticEngine()
 
     var company: Company
 
@@ -30,6 +32,7 @@ struct ResultView: View {
         .colorScheme(.light)
         .navigationTitle(company.name)
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: performHaptics)
     }
 
     // MARK: - Components
@@ -89,13 +92,39 @@ struct ResultView: View {
             .padding(16.0)
     }
 
-    // MARK: - Debug
-    private func createDebug() -> some View {
-        HStack {
-            Text("#: \(company.hashScore) |")
-            Text("@: \(company.arobaseScore) |")
-            Text("news: \(company.newsScore) |")
-         }
+    // MARK: - Helper functions
+    func performHaptics() {
+        do {
+            try engine?.start()
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+            let parameter = CHHapticParameterCurve(
+                parameterID: .hapticIntensityControl,
+                controlPoints: [start, end],
+                relativeTime: 0
+            )
+            let event1 = CHHapticEvent(
+                eventType: .hapticTransient,
+                parameters: [intensity, sharpness],
+                relativeTime: 0
+            )
+
+            let event2 = CHHapticEvent(
+                eventType: .hapticContinuous,
+                parameters: [sharpness, intensity],
+                relativeTime: 0.125,
+                duration: 1
+            )
+
+            let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Haptics failed: \(error.localizedDescription)")
+        }
     }
 
 }
